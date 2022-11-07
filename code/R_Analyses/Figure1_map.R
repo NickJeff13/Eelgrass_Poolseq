6c1c1bdbc9ba4c20eb515df70d0edb51346fd6d6
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2022-11-07 09:56:20 -0400

    updated code to show fine-scale insets

--- code/R_Analyses/Figure1_map.R
+++ code/R_Analyses/Figure1_map.R
@@ -14,6 +14,7 @@ sf_use_s2(FALSE) #makes some of the sf functions run a bit less buggy
 #Projections ------------
 latlong <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
 CanProj <- "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=63.390675 +lon_0=-91.86666666666666 +x_0=6200000 +y_0=3000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+utm <- "+proj=utm +zone=20 +datum=NAD83 +units=km +no_defs +ellps=GRS80 +towgs84=0,0,0"
 
 #colours for the points
 colour_df <- data.frame(code=c("NAH","GRB","PORT","HEB","PRJ","SAC","MASI","SAM","L3F","TAYH","POUL","EBAY","NRIV",
@@ -45,6 +46,8 @@ write.csv(colour_df,"Data/Colour_codes.csv",row.names=FALSE)
 
 #Shapefiles
 can_eez <- read_sf("r:/Science/CESD/HES_MPAGroup/Data/Shapefiles/Canada_EEZ.shp")%>%st_transform(CanProj)
+
+ns_coast <- read_sf("r:/Science/CESD/HES_MPAGroup/Data/Shapefiles/Coastline/NS_coastline_project_Erase1.shp")%>%st_transform(CanProj)
 
 #create a map boundary for the analysis based on the Canadian EEZ
 can_bounding <- can_eez%>%
@@ -151,3 +154,48 @@ inset <- ggplot()+
 
 ggsave("Figures/atlantic_point_map.png",atlantic,height=8,width=6,units="in",dpi=300)
 ggsave("Figures/eelgrass_point_map_inset.png",inset,height=8,width=8,units="in",dpi=300)
+
+#micro inset map
+
+#MASI-SAC
+masi_inset_bounds <- eelgrass_coords%>%
+                     filter(code %in% c("MASI","SAC"))%>%
+                     st_bbox()%>%
+                     st_as_sfc()%>%
+                     st_as_sf()%>%
+                     st_buffer(5*1000)%>%
+                     st_bbox()
+masi_sac <- ggplot()+
+            geom_sf(data=ns_coast,lwd=0,col=NA,fill="grey60")+
+            geom_sf(data=eelgrass_coords%>%filter(code %in% c("MASI","SAC")),aes(fill=col),shape=21,size=4)+
+            coord_sf(expand=0,xlim=masi_inset_bounds[c(1,3)],ylim=masi_inset_bounds[c(2,4)])+
+            theme_bw()+
+            scale_fill_identity()+
+            theme(legend.position = "none")+
+            annotation_scale(location="br")
+
+
+#get high resolution coastline data for James bay - https://maps-cartes.ec.gc.ca/arcgis/rest/services/ShorelineMappingNWRC_CartographieDuLittoralCNRF/MapServer
+
+jb_link <- "https://maps-cartes.ec.gc.ca/arcgis/rest/services/ShorelineMappingNWRC_CartographieDuLittoralCNRF/MapServer/2"
+jb_link <- "https://maps-cartes.ec.gc.ca/arcgis/rest/services/ShorelineMappingNWRC_CartographieDuLittoralCNRF/MapServer/3"
+
+jb_sf <- esri2sf::esri2sf(paste0(jb_link, "0"))
+st_write(jb_sf,"Data/James_Bay.shp")
+
+jb_inset_bounds <- eelgrass_coords%>%
+                  filter(code %in% c("JB33","JB38"))%>%
+                  st_bbox()%>%
+                  st_as_sfc()%>%
+                  st_as_sf()%>%
+                  st_buffer(15*1000)%>%
+                  st_bbox()
+
+jb_inset <- ggplot()+
+  geom_sf(data=Canada,lwd=0,col=NA,fill="grey60")+
+  geom_sf(data=eelgrass_coords%>%filter(code %in% c("JB33","JB38")),aes(fill=col),shape=21,size=4)+
+  coord_sf(expand=0,xlim=jb_inset_bounds[c(1,3)],ylim=jb_inset_bounds[c(2,4)])+
+  theme_bw()+
+  scale_fill_identity()+
+  theme(legend.position = "none")+
+  annotation_scale(location="br")
